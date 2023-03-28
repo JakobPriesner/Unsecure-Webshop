60a2be0882e7f7c7696687d891c7af77a8da71d6
Jakob Priesner <jakob.priesner@outlook.de>
2023-03-28 10:36:25 +0200

    refactor: improve project structure

---
+++ backend/src/main/java/de/fhws/biedermann/webshop/api/services/ContactService.java
@@ -0,0 +1,31 @@
+package de.fhws.biedermann.webshop.api.services;
+
+import de.fhws.biedermann.webshop.api.states.ContactState;
+import de.fhws.biedermann.webshop.models.Contact;
+
+import jakarta.servlet.http.HttpServletRequest;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+
+@Path("contact") public class ContactService
+{
+	@Context protected UriInfo uriInfo;
+
+	@POST @Consumes(MediaType.APPLICATION_JSON) public Response createContact(
+		final Contact contact,
+		@HeaderParam( "uuid" ) final String uuid,
+		@Context UriInfo uriInfo,
+		@Context HttpServletRequest request
+	)
+	{
+		return new ContactState.Builder()
+			.withUuid( uuid )
+			.withModel( contact )
+			.defineResponseBody( null )
+			.build( )
+			.noContent( );
+	}
+}
