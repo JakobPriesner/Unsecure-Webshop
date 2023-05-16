54a2d3f2e455a35d456a5ae119bdc856d0350c0e
Jakob Priesner <jakob.priesner@outlook.de>
2023-05-16 11:30:58 +0200

    test ci with always failing test

--- test/utils/logic/AlwaysFailingTest.java
+++ test/utils/logic/AlwaysFailingTest.java
@@ -1,19 +1,13 @@
 package utils.logic;
 
-import de.fhws.biedermann.webshop.utils.logic.OrderLogic;
 import org.junit.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 
-import javax.ws.rs.BadRequestException;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
+import static org.junit.Assert.*;
 
 
-public class OrderLogicTest {
+public class AlwaysFailingTest {
     @Test
-    public void testGetShippingCostsEmptyCountry() {
-        assertTrue(false);
+    public void testFailing() {
+        fail();
     }
 }
